da53c948626fb612b662a02368c1274724dea995
atakanreyhanioglu <atakan.reyhanioglu@gmail.com>
2022-03-12 16:17:02 +0300

    marker content added

--- src/Map.ts
+++ src/Map.ts
@@ -3,6 +3,7 @@ interface Mappable {
         lat: number,
         lng: number
     }
+    markerContent(): string
 }
 
 export class Map {
@@ -18,12 +19,19 @@ export class Map {
         })
     }
     addMarker(mappable: Mappable): void {
-        new google.maps.Marker({
+        const marker = new google.maps.Marker({
             map: this.googleMap,
             position: {
                 lat: mappable.location.lat,
                 lng: mappable.location.lng
             }
+        })
+        marker.addListener('click', () => {
+            const infoWindow = new google.maps.InfoWindow({
+                content: mappable.markerContent()
+            });
+
+            infoWindow.open(this.googleMap, marker)
         })
     }
 }
